b39910f64ba8966f7a6e01fce16cea91671fdb58
EmmanuelVegaLira <emmanuelvegalira@gmail.com>
2024-04-25 11:45:11 -0600

    commit a docker-repo

--- docker-intro/index.js
+++ docker-intro/index.js
@@ -11,7 +11,7 @@ const app = express()
 mongoose.connect('mongodb://vega:password@monguito:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos...')
+  console.log('listando... Chanchito...')
   const animales = await Animal.find();
   return res.send(animales)
 })
